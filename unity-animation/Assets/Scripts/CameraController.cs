e1593e935238ed722bcd8c10f08ddde8102a1fea
Coleraines13 <nttreaper@gmail.com>
2023-07-26 05:54:27 -0700

    early bird gets the animation done

--- unity-animation/Assets/Scripts/CameraController.cs
+++ unity-animation/Assets/Scripts/CameraController.cs
@@ -1,27 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-    private Transform t;
+    
     private Vector3 offset;
 
     public GameObject player;
     public float turnSpeed = 5.0f;
 
+    public Toggle InvertedYMode;
+    public bool isInverted = false;
+    private int inverted;
+
     // Start is called before the first frame update
     void Start()
     {
-        t = GetComponent<Transform>();
-        offset = t.position - player.transform.position;
+        if (InvertedYMode)
+            isInverted = true;
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.left) * offset;
-        t.position = player.transform.position + offset;
+        if (isInverted)
+                    inverted = -1;
+        else
+                    inverted = 1;
+        //If tap button rigth of mouse is actived rotation
+        if(Input.GetMouseButton(1))
+        {
+            Debug.Log("Active Rotation");
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis((Input.GetAxis("Mouse Y") * inverted) * turnSpeed, Vector3.left) * offset;
+        transform.position = player.transform.position + offset;
         transform.LookAt(player.transform.position);
+        }
+        else
+        {
+            Debug.Log("Desactive Rotation");
+            transform.position = player.transform.position + offset;
+        }
     }
 }
