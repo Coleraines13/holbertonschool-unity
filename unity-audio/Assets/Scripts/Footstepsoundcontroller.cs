4c49c2b9016f14078fc025a50d42205fdd7aa7c2
Cole raines <98409647+Coleraines13@users.noreply.github.com>
2023-08-21 17:51:14 -0500

    oooof

---
+++ unity-audio/Assets/Scripts/Footstepsoundcontroller.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Footstepsoundcontroller : MonoBehaviour
+{
+    public AudioClip footstepsRunningGrass; // Sound for running on grass
+    public AudioClip footstepsRunningRock;  // Sound for running on rock
+    public AudioSource audioSource; // Reference to the AudioSource component
+    public CharacterController characterController; // Reference to the CharacterController component
+    public Animator animator; // Reference to the Animator component
+
+    private bool isGrounded = true; // Flag to track if the player is grounded
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Check if the player is grounded
+        isGrounded = characterController.isGrounded;
+
+        // Play footstep sound when the player is grounded and moving
+        if (isGrounded && characterController.velocity.magnitude > 0.1f)
+        {
+            if (animator.GetBool("IsRunning"))
+            {
+                PlayFootstepSound();
+            }
+        }
+    }
+
+    private void PlayFootstepSound()
+    {
+        if (!audioSource.isPlaying)
+        {
+            if (animator.GetBool("IsOnGrass"))
+            {
+                audioSource.clip = footstepsRunningGrass;
+            }
+            else if (animator.GetBool("IsOnRock"))
+            {
+                audioSource.clip = footstepsRunningRock;
+            }
+
+            audioSource.Play();
+        }
+    }
+}
